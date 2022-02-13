8c8396c5b0234117b9d24f86c59e104485aa5bef
Maheswararao Ijjurothu <mrijjurothu@taxilla.com>
2022-02-14 00:32:05 +0530

    user and upload user related changes done

--- comp-chat/src/app/components/users/users.component.ts
+++ comp-chat/src/app/components/users/users.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { User } from 'src/app/models/user';
+import { AdminService } from 'src/app/service/admin.service';
+import { CommonUtilsService } from 'src/app/services/common-utils.service';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +11,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
+  destroy$: Subject<boolean> = new Subject<boolean>();
+  filterString: string = '';
+  showTemplate: boolean = true;
+  showSlideTemplate: string = "USERS";
+  users: User[] = [];
 
-  constructor() { }
+  constructor(
+    private commonUtils: CommonUtilsService,
+    private adminService: AdminService
+  ) { }
 
   ngOnInit(): void {
+    this.adminService.users.pipe(
+      takeUntil(this.destroy$))
+      .subscribe(result => {
+        this.users = result;
+      });
+
+    this.getUsers();
   }
 
+  getUsers = () => {
+    this.adminService.getUsers(() => { });
+  }
+
+  applyFilter(event: any) {
+    this.filterString = event.target.value;
+  }
+
+  add = (drawer: any) => {
+    this.showSlideTemplate = "IMPORT_USER";
+    this.slide(drawer);
+  }
+
+  slide = (drawer: any) => {
+    if (drawer._opened) {
+      this.showTemplate = true;
+    } else {
+      this.showTemplate = false;
+    }
+    drawer.toggle();
+  }
+
+  ngOnDestroy() {
+    this.adminService.users.next([]);
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
 }
