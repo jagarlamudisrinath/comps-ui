b579543483a7cce7d3faccb342a97097b5b61256
Maheswararao Ijjurothu <mrijjurothu@taxilla.com>
2022-02-15 17:19:08 +0530

    user information added in header and logout added

--- comp-chat/src/app/components/header/header.component.ts
+++ comp-chat/src/app/components/header/header.component.ts
@@ -15,7 +15,8 @@ export class HeaderComponent implements OnInit {
   constructor(public rootScope: RootScopeService, private commonUtils: CommonUtilsService) { }
 
   ngOnInit(): void {
-    this.rootScope.IS_USER_LOGGED_IN.next(Boolean(localStorage.getItem('IS_USER_LOGGED_IN')));
+    let isUserLoggedIn = localStorage.getItem('IS_USER_LOGGED_IN') === null ? false : Boolean(localStorage.getItem('IS_USER_LOGGED_IN'));
+    this.rootScope.IS_USER_LOGGED_IN.next(isUserLoggedIn);
     let localUser = localStorage.getItem('LOGGED_IN_USER');
     let user = localUser ? JSON.parse(localUser) : new User();
     this.rootScope.LOGGED_IN_USER.next(user);
